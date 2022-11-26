b6cce91e150532f8c22db8e629bc179466a6086e
Mtsumi <mtsumijr@gmail.com>
2022-11-26 22:17:09 +0300

    setting up index page

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask,render_template
+
 app = Flask(__name__) 
 
-@app.route("/www.maskani.tech/login")
-def login_page():
-    email = 
-    password =
-    return render_template("pages/template/index.html",)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+# @app.route("/www.maskani.tech/login")
+# def login_page():
+#     email = 
+#     password =
+#     return render_template("pages/template/index.html",)
