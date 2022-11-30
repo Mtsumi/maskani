3a8dc05728b3241d9fd6973a83778537f0a79784
hillaryoyaro <hillaryoyaroh@gmail.com>
2022-11-30 13:44:37 +0300

    update of tempate

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,BooleanFieldfrom
-from wtforms.validators import InputRequired,Email,lentgh
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy  import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Thisisasecret!' 
-app.config.from_object('config')
 
-class LoginForm(FlaskForm):
-    username = StringField('username')
-    password = PasswordField('password')
 
-@app.route('/form', methods=['GET', 'POST'])
-def form():
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        return '<h1>The username is {}. The password is {}.'.format(form.username.data, form.password.data)
-    return render_template('form.html', form=form)
-
-@app.route("/")
+@app.route('/')
 def index():
+<<<<<<< HEAD
     return render_template("pages/index.html")
+=======
+    return render_template('index.html')
+
+>>>>>>> 08326fc (update of tempate)
 
 if __name__ == '__main__':
     app.run(debug=True)
