4a098d7dce6e4069581654d6d95e0218c0d68e5a
hillaryoyaro <hillaryoyaroh@gmail.com>
2022-12-02 14:32:05 +0300

    upgrade

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm 
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
-from flask_sqlalchemy  import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask import Flask,render_template
 
-app = Flask(__name__)
+app = Flask(__name__) 
+app.config.from_object('config')
 
 
-@app.route('/')
+@app.route("/")
 def index():
-<<<<<<< HEAD
     return render_template("pages/index.html")
-=======
-    return render_template('index.html')
 
->>>>>>> 08326fc (update of tempate)
 
-if __name__ == '__main__':
-    app.run(debug=True)
