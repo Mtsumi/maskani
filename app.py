ebf7d4bdf9f7e3d7f0a88bfa51b0f1c49ae559df
Mtsumi <mtsumijr@gmail.com>
2022-12-09 12:17:11 +0300

    Completed Fundi Signup route

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user
 #, login_required, logout_user, current_user
-from .forms import *
+from .forms import ClientRegisterForm, FundiRegisterForm
 from . import app, db
 
 
@@ -35,6 +35,26 @@ def sign_up():
     flash('Something went wrong with validation')
     return render_template('sign_up.html', title='Register', form=form)
 
+
+@app.route('/fundis/sign_up', methods=['GET', 'POST'])
+def fundi_signup():
+    form = FundiRegisterForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = Fundi(
+                first_name=form.first_name.data,
+                last_name=form.last_name.data,
+                email=form.email.data,
+                password=hashed_password,
+                profession=form.profession.data,
+                )
+        db.session.add(new_user)
+        db.session.commit()
+        #login_user(new_user, remember=True)
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('edit_fundi'))
+    flash('Something went wrong with validation')
+    return render_template('fundi_sign_up.html', title='Register As A Fundi', form=form)
         
 
 @app.route("/sign")
@@ -53,3 +73,10 @@ def about():
 def edit_client():
     return "<h1> This is the where client comes after login <h1>"
 
+@app.route("/fundis/edit")
+def edit_fundi():
+    return "<h1> This is the where Fundi comes after login <h1>"
+
+@app.route("/fundis/login")
+def fundi_login():
+    return "<h1>This is the login route for fundis</h1>"
