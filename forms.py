3f169eafe768600a9dcaf1617292688b060afc89
Mtsumi <mtsumijr@gmail.com>
2022-12-11 22:52:28 +0300

    Included a User model that manages login and Registration

--- forms.py
+++ forms.py
@@ -17,32 +17,18 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
     
-class ClientRegisterForm(FlaskForm):
+class RegisterForm(FlaskForm):
     
     first_name = StringField('First Name', validators=[InputRequired(), Length(min=4, max=15)])
     last_name = StringField('Last Name', validators=[InputRequired(), Length(min=4, max=15)])
     email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=200)])
+    choices = [('client', 'Client'), ('fundi', 'Fundi')]
+    role = SelectField('Role', validators=[DataRequired()], choices=choices)
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
     def validate_email(self, email):
-        user = Client.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
-
-class FundiRegisterForm(FlaskForm):
-
-    first_name = StringField('First Name', validators=[InputRequired(), Length(min=4, max=15)])
-    last_name = StringField('Last Name', validators=[InputRequired(), Length(min=4, max=15)])
-    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
-    choices = [('plumber', 'Plumber'), ('electrician', 'Electrician'), ('painter', 'Painter'), ('mason', 'Mason'), ('carpenter', 'Carpenter')]
-    profession = SelectField('Profession', validators=[DataRequired()], choices=choices)
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
-
-    def validate_email(self, email):
-        user = Fundi.query.filter_by(email=email.data).first()
+        user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
