e2f4fe7dec835e130b3e7721622eabfc98c9abc0
Mtsumi <mtsumijr@gmail.com>
2022-12-12 22:38:09 +0300

    Same error, different day

--- app.py
+++ app.py
@@ -16,16 +16,6 @@ db.create_all()
 def index():
     return render_template("pages/index.html")
 
-@app.route("/dashboard")
-def dashboard():
-    if current_user.is_authenticated:
-        user = current_user
-        if user.user.role == 'fundi':
-            return redirect(url_for('mywork'))
-        else:
-            return redirect(url_for('myorders'))
-    return("<h1>There is no User here!<h2>")
-
 @app.route("/sign_up", methods=['GET', 'POST'])
 def sign_up():
     if current_user.is_authenticated:
@@ -40,7 +30,7 @@ def sign_up():
                     role=form.role.data)
         db.session.add(user)
         db.session.commit()
-        print(f'I am a ' + {user.role})
+        #print(f'I am a ' + {user.role})
         if user.role=='client': 
             new_client = Client(user_id=user.id)
             print('Creating a "client" object and logging the user in')
@@ -69,13 +59,38 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('dashboard'))
-            
-        else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
+            user_id = user.id
+            if user.role == 'fundi':
+                
+                fundi = Fundi.query.filter_by(user_id=user_id).first()
+                print(fundi)
+                login_user(fundi, remember=form.remember.data)
+                next_page = request.args.get('next')
+                return redirect(next_page) if next_page else redirect(url_for('dashboard'))
+                
+            else:
+                client = Client.query.filter_by(user_id=user_id).first()
+                login_user(client, remember=form.remember.data)
+                next_page = request.args.get('next')
+                return redirect(next_page) if next_page else redirect(url_for('dashboard'))
+        flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Maskani Login', form=form)
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    if current_user.is_authenticated:
+        if current_user.user.role == 'fundi':
+            print(current_user.user.last_name)
+            print(current_user.user.role)
+            return redirect(url_for('mywork'))
+        else:
+            print(current_user.user.last_name)
+            print(current_user.user.role)
+            return redirect(url_for('myorders'))
+        
+    return("<h1>There is no User here!<h2>")
+
 
 @app.route("/clients/myorders")
 @login_required
