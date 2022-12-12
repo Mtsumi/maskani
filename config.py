88b8d9dcc5adb953afe2cd31a46a850c0b929dbd
Mtsumi <mtsumijr@gmail.com>
2022-12-12 12:51:54 +0300

    config with database

--- config.py
+++ config.py
@@ -11,13 +11,8 @@ class Config(object):
 
     # Connect to the database
     DATABASE_NAME = 'maskani'
-<<<<<<< HEAD
     username = 'oyaro'
     password = 'serverless'
-=======
-    username = 'postgres'
-    password = 'ikxi0000'
->>>>>>> main
     url = 'localhost:5432'
     SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(
         username,
@@ -28,11 +23,4 @@ class Config(object):
 
     # CSRF token with wtforms
     #WTF_CSRF_ENABLED = True
-<<<<<<< HEAD
-<<<<<<< HEAD
-    #WTF_CSRF_SECRET_KEY = 'wtf secret key'
-=======
-    #WTF_CSRF_SECRET_KEY = 'wtf secret key'
->>>>>>> main
-=======
     #WTF_CSRF_SECRET_KEY = 'wtf secret key'
